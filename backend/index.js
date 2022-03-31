83100e0852fbdb6f0d8f7f7f974766466d531f0b
ANUPAM-RO <68887502+ANUPAM-RO@users.noreply.github.com>
2022-03-31 13:23:09 +0530

    Modify

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,54 @@
-const express = require('express')
+const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const authRoute = require("./routes/UserRoute")
-const cors=require("cors");
-app.use(express.json())
-mongoose.connect("mongodb://localhost:27017/chat",{
+const http = require("http");
+const authRoute = require("./routes/UserRoute");
+const cors = require("cors");
+const { Server, Socket } = require("socket.io");
+app.use(express.json());
+mongoose.connect(
+  "mongodb://localhost:27017/chat",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology:true,
-
-}, (err)=>{
-    if (err){
-        console.log(err);
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Mongodb connected Sucessfully");
     }
-    else{
-        console.log("Mongodb connected Sucessfully");
-    }
+  }
+);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-
-app.use(cors())
-app.get("/", (req, res) => {
-    res.send("hello")
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with id: ${socket.id} joined room: ${data}`);
+    });
+    socket.on("send_message", (data) => {
+        console.log(data);
+        socket.to(data.room).emit("receive_message", data);
+    })
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
 });
+
+app.use(cors());
 
 app.use("/auth", authRoute);
-app.listen(8000, ()=>{
-    console.log("Backend server is running ")
+app.listen(8000, () => {
+  console.log("Backend server is running ");
+});
+server.listen(9000, () => {
+  console.log("SERVER RUNNING");
 });
